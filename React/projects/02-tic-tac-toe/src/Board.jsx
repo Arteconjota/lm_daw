f73cb963d91f0954fa08028d8826f4c71f8048f8
Javier Piñero <javier.piserrano@gmail.com>
2025-06-04 12:43:33 +0200

    Restricciones Tic Tac Toe

--- React/projects/02-tic-tac-toe/src/Board.jsx
+++ React/projects/02-tic-tac-toe/src/Board.jsx
@@ -15,9 +15,15 @@ function Board() {
   console.log("render board")
   //const board = Array(9).fill(null);
   //const board = ["X", "O", "X", "X", "O", "X", "X", "O", "X"];
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const localStorageBoard = window.localStorage.getItem('board')
+    return localStorageBoard ? JSON.parse(localStorageBoard) : Array(9).fill(null)
+  });
 
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnLocalStorage = window.localStorage.getItem('turn')
+    return turnLocalStorage ? turnLocalStorage : TURNS.X
+  });
 
   //El ganador por defecto está a null, solo cambia si encontramos un ganador
   const [winner, setWinner] = useState(null);
@@ -55,6 +61,10 @@ function Board() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
+    // Guardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard));
+    window.localStorage.setItem('turn', newTurn);
+
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
       confetti()
@@ -70,6 +80,11 @@ function Board() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    //window.localStorage.clear(); Se puede utilizar, pero en proyectos amplios, puede eliminar más de lo necesario.
+
+    window.localStorage.removeItem('board');
+    window.localStorage.removeItem('turn');
   }
 
   const checkEndGame = (newBoard) => {
